0fa3ac14c9292524cf97db87c417089aa39dc01e
ogir-39 <tranphuongvy03092005@gmail.com>
2024-08-29 18:11:44 +0700

    Update index.js

--- javascript/index.js
+++ javascript/index.js
@@ -1085,6 +1085,15 @@ window.onload = function () {
 
 
     }
-
-
+    $('.subMenuP').hide();
+    if (window.innerWidth<=500) {
+        
+    $('.menuPhone').on('click',function(){
+        console.log(1);
+        $('.subMenuP').show();
+        $('.subMenuP>li:first-child>i').on('click',function(){
+            $('.subMenuP').hide();
+        })
+    })
+    }
 }
